a31b77e2ad098d3bd2fce5ea59e88ef1c6c330c7
LawrenceOnuoha <44349202+LawrenceOnuoha@users.noreply.github.com>
2023-07-11 22:29:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 let count = 0;
- 
+
 function localMiddleware(req, res, next) {
     count += 1;
 
@@ -15,10 +15,10 @@ function localMiddleware(req, res, next) {
 
 // Data
 let students = [
-    { id: 101 , name: 'Student1 Aus', section: 'Computer Science', gpa: '3.72', nationality: 'Australian' },
-    { id: 102 , name: 'Student2 Braz', section: 'Music', gpa: '3.95', nationality: 'Brazilian' },
-    { id: 103 , name: 'Student3 Ice', section: 'History', gpa: '2.84', nationality: 'Icelander' },
-    { id: 104 , name: 'Student4 Ken', section: 'Web Development', gpa: '3.68', nationality: 'Kenyan' }
+    { id: 101, name: 'Student1 Aus', section: 'Computer Science', gpa: '3.72', nationality: 'Australian' },
+    { id: 102, name: 'Student2 Braz', section: 'Music', gpa: '3.95', nationality: 'Brazilian' },
+    { id: 103, name: 'Student3 Ice', section: 'History', gpa: '2.84', nationality: 'Icelander' },
+    { id: 104, name: 'Student4 Ken', section: 'Web Development', gpa: '3.68', nationality: 'Kenyan' }
 ]
 
 // Create a new student
@@ -35,16 +35,30 @@ app.get('/students', localMiddleware, (req, res) => {
 
 // Filtering student records based on ID or Section
 
-app.get('/students/filter', function(req, res) {
-    let studId = parseInt(req.query.id); //parseInt converts a string to integer.
-    let studSec = req.query.section;
+app.get('/students/filter', function (req, res) {
+    if (req.query.id !== undefined) {
+        let studId = parseInt(req.query.id); //parseInt converts a string to integer.
 
-    // Filtering out students that correspond to specific id.
-    let result = students.filter((item)=> {
-        return item.id == studId;
-    })
-    res.send(result);
-    
+        // Filtering out students that correspond to specific id.
+        let result = students.filter((item) => {
+            return item.id == studId;
+        });
+
+        res.send(result);
+    }
+
+    if (req.query.section !== undefined) {
+        let section = req.query.section;
+
+        // Filtering out students based on section.
+        let result = students.filter((item) => {
+            return item.section == section;
+        });
+
+        res.send(result);
+    }
+
+
 });
 
 // Full student record update
@@ -59,7 +73,7 @@ app.put('/students/:id', localMiddleware, function (req, res) {
 
     // Update student data
     let newStudentData = req.body;
-    students [index] = newStudentData;
+    students[index] = newStudentData;
 
     res.send(newStudentData);
 });
